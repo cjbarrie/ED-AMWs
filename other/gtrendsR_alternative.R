9444d8227fdce330657671ab4e2d48419985c501
cjbarrie <chrisjbarrie@googlemail.com>
2023-01-31 11:55:11 +0000

    Update alternative files

--- other/gtrendsR_alternative.R
+++ other/gtrendsR_alternative.R
@@ -7,6 +7,9 @@ data("categories") # get numbers of all categories to filter data
 
 # Simple call
 
+#find alternative gtrends() function to deal with 492 errors at:
+# https://raw.githubusercontent.com/cjbarrie/ED-AMWs/main/other/gtrends_with_backoff.R
+
 dat <- gtrends_with_backoff("racism",
                time = "2020-01-01 2020-08-22",
                geo = "GB",
